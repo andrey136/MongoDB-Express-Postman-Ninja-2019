fc2908e8933564cd9b115796feff56a7b3feaa48
andrey_mardash <andrey.mardash@gmail.com>
2021-05-12 06:49:53 +0300

    Added new api  Ninja.aggregate() and Ninja.findByIdAndUpdate() in routes/api.js

--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,19 @@ const Ninja = require('../models/ninja');
 
 // get a list of ninja from the db
 router.get('/ninjas', (req, res, next) => {
-  res.send({type: "GET"});
+  Ninja.aggregate([
+    {
+      $geoNear: {
+        near: {type:'Point',coordinates:[parseFloat(req.query.lng), parseFloat(req.query.lat)]},
+        distanceField: "dist.calculated",
+        maxDistance: 100000,
+        spherical: true
+      }
+    }
+  ])
+    .then(function(result){
+      res.send(result);
+    }).catch(next);
 });
 
 // add a new ninja to the db
@@ -16,13 +28,16 @@ router.post('/ninjas', (req, res, next) => {
 
 // update a ninja in the db
 router.put('/ninjas/:id', (req, res, next) => {
-  res.send({type: "PUT"});
+  Ninja.findByIdAndUpdate({_id: req.params.id}, req.body).then((ninja) => {
+    Ninja.findOne({_id: req.params.id}).then((ninja) => {
+      res.send(ninja);
+    });
+  });
 });
 
 // delete a ninja from the db
 router.delete('/ninjas/:id', (req, res, next) => {
   Ninja.findByIdAndRemove({_id: req.params.id}).then((ninja) => {
-    console.log('!!!!!!!!!');
     res.send(ninja);
   });
 });
